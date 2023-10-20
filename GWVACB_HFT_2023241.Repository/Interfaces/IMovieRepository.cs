00d5d1e16cb8772ee5e2dcf4229945cd1d52d960
RED <letsupdate9@gmail.com>
2023-10-20 13:18:40 +0200

    Added Dependencies

---
+++ GWVACB_HFT_2023241.Repository/Interfaces/IMovieRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GWVACB_HFT_2023241.Repository.Interfaces
+{
+    internal interface IMovieRepository:IRepository<Movie>
+    {
+    }
+}
---
+++ GWVACB_HFT_2023241.Repository/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GWVACB_HFT_2023241.Repository.Interfaces
+{
+    internal interface IRepository<T>
+    {
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        T GetById(int id);
+        IQueryable<T> GetAll();
+    }
+}
