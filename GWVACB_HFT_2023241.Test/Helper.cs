209f3fa2aee96ab846a50a754a32efcf7f277838
RED <letsupdate9@gmail.com>
2024-05-07 18:34:00 +0200

    unnecessary code

--- GWVACB_HFT_2023241.Test/Helper.cs
+++ GWVACB_HFT_2023241.Test/Helper.cs
@@ -8,28 +8,6 @@ namespace GWVACB_HFT_2023241.Test
 {
     public class Helper
     {
-        private static bool ValidateObjectByAttributes(object obj)
-        {
-            var objectType = obj.GetType();
-            var properties = objectType.GetProperties();
-
-            foreach (var property in properties)
-            {
-                var attributes = property.GetCustomAttributes();
-
-                foreach (var attribute in attributes)
-                    if (attribute is ValidationAttribute validationAttribute)
-                    {
-                        var value = property.GetValue(obj);
-
-                        if (!validationAttribute.IsValid(value))
-                            return
-                                false; // Ha bármely attribútum ellenőrzése nem felel meg, akkor hamis értékkel tér vissza.
-                    }
-            }
-
-            return true; // Ha az összes attribútum ellenőrzése sikeres volt, akkor igaz értékkel tér vissza.
-        }
 
         private static MockDataSet _dataSet;
         public static MockDataSet GetMocDataset()
